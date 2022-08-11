b7a78b5608423ad29f7a4d153f5f07d2b67252cd
bentleycodes <brandonprasouvo@live.com>
2022-08-10 20:02:50 -0700

    Feedback after presentation.

--- js/script.js
+++ js/script.js
@@ -1,42 +1,34 @@
 // 8 Ball images array.
 const imgArray = [];
-imgArray[0] = new Image();
-imgArray[0].src = '8ball-images/asiseeitball.png',
-imgArray[1] = new Image();
-imgArray[1].src = '8ball-images/askagainball.png',
-imgArray[2] = new Image();
-imgArray[2].src = '8ball-images/cannotpredictball.png',
-imgArray[3] = new Image();
-imgArray[3].src = '8ball-images/concentrateball.png',
-imgArray[4] = new Image();
-imgArray[4].src = '8ball-images/dontcountball.png',
-imgArray[5] = new Image();
-imgArray[5].src = '8ball-images/itiscertainball.png',
-imgArray[6] = new Image();
-imgArray[6].src = '8ball-images/noball.png',
-imgArray[7] = new Image();
-imgArray[7].src = '8ball-images/verydoubtfulball.png',
-imgArray[8] = new Image();
-imgArray[8].src = '8ball-images/yesball.png';
+imgArray[0] = '8ball-images/asiseeitball.png',
+imgArray[1] = '8ball-images/askagainball.png',
+imgArray[2] = '8ball-images/cannotpredictball.png',
+imgArray[3] = '8ball-images/concentrateball.png',
+imgArray[4] = '8ball-images/dontcountball.png',
+imgArray[5] = '8ball-images/itiscertainball.png',
+imgArray[6] = '8ball-images/noball.png',
+imgArray[7] = '8ball-images/verydoubtfulball.png',
+imgArray[8] = '8ball-images/yesball.png';
 
 // Event listener for the ask button. Includes an edge case for invalid questions & will show question + ball image.
 askbtn.addEventListener('click',function(e) {
+  e.preventDefault();
     if(!question.value || question.value.length < 5) {
       alert("Please provide a question.");
-      e.preventDefault();
       return;
-    }
-    e.preventDefault();  
+    } 
      askbtn.value = "ask again";
      showquestion.innerHTML = `YOUR QUESTION: ${question.value}`;
      showquestion.style.visibility = 'visible';
      displayImage();
      ball.animate(
       [
+        {opacity: '0'},
         {transform: 'translateY(-10%)'},
-        {transition: '1s ease'}
+        {transition: '1s ease'},
+        {opacity: '1'}
    ], {
-     duration: 500,
+     duration: 1000,
      iterations: 1
    }  
    );
@@ -49,8 +41,7 @@ ball.addEventListener("click", clearInput);
 // Function to get a random image then display.  
     function displayImage() {
         const randomNum = Math.floor(Math.random() * (imgArray.length));
-        const img = imgArray[randomNum].src;
-        ball.src = img;
+        ball.src = imgArray[randomNum];
         ball.style.visibility = 'visible';
     }
 
@@ -63,6 +54,6 @@ ball.addEventListener("click", clearInput);
 
       setTimeout(() => {
         location.reload();
-      }, "1100");
+      }, "1200");
     
     }
